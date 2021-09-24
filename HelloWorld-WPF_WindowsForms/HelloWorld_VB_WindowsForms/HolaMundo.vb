efabcae642df7babba8fba19f46bada116eab715
Alejandro Ruiz Campos <alexruiz29del12@gmail.com>
2021-09-24 12:45:22 +0200

    VB y C#
    Hola mundo en visual basic y c#

---
+++ HelloWorld-WPF_WindowsForms/HelloWorld_VB_WindowsForms/HolaMundo.vb
@@ -0,0 +1,32 @@
+﻿Public Class HolaMundo
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+
+
+    End Sub
+
+    Private Sub HolaMundo_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim name As String
+
+        name = CajaTexto.Text
+
+        MessageBox.Show("Hola " + name)
+        'otra forma MsgBox("Hola " + name)'
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles CajaTexto.TextChanged
+
+    End Sub
+End Class
