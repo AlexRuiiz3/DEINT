8a850a522ec9d936ea2bff27aed72e87fac88b91
Alejandro Ruiz Campos <alexruiz29del12@gmail.com>
2021-10-25 14:37:14 +0200

    Unidad 10 Act 1 y 2

--- Unidad9/Actividad3/App.xaml.cs
+++ Unidad9/Actividad3/App.xaml.cs
@@ -66,7 +66,7 @@ namespace Actividad3
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(BlankPage1), e.Arguments);
+                    rootFrame.Navigate(typeof(MainPageGrid), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
