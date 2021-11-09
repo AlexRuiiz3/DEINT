d0775469aca1032bb2eda98454df6917323413e0
Alex <alejandro.ruiz@iesnervion.es>
2021-11-09 21:27:32 +0100

    Unidad 11 Act 1

--- Unidad11/Actividad1/ViewModels/MainPageVM.cs
+++ Unidad11/Actividad1/ViewModels/MainPageVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,24 +8,38 @@ using Actividad1.ViewModels.Utilidades;
 using Dal;
 using Entidades;
 using Windows.UI.Xaml;
-
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
 
 namespace Actividad1.ViewModels
 {
     public class MainPageVM : clsVMBase
     {
-        private List<ClsPersona> listadoPersonas = GestoraPersonas.obtenerPersonas();
+        private ObservableCollection<ClsPersona> listadoPersonas = GestoraPersonas.obtenerPersonas();
 
-        public List<ClsPersona> ListadoPersonas {
-            get { return listadoPersonas; }
-            set { listadoPersonas = value; }
-        }
+        public ObservableCollection<ClsPersona> ListadoPersonas { get { return listadoPersonas; } set { listadoPersonas = value; } }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void button_EliminarPersona(object sender, RoutedEventArgs e)
+        {
+            bool eliminada = false;
+            ClsPersona persona = (ClsPersona)((AppBarButton)sender).Tag;
 
-        public void button_EliminarPersona(object sender, RoutedEventArgs e) {
-            if (ListadoPersonas.Count > 0) {
-                ListadoPersonas.RemoveAt(ListadoPersonas.Count - 1);
-                NotifyPropertyChanged("ListadoPersonas");
+            if (persona != null)
+            {
+                for (int i = 0; i < listadoPersonas.Count && !eliminada; i++)
+                {
+                    if (persona.Equals(listadoPersonas.ElementAt(i)))
+                    {
+                        listadoPersonas.RemoveAt(i);
+                        eliminada = true;
+                    }
+                }
             }
+
         }
     }
 }
--- Unidad11/Dal/GestoraPersonas.cs
+++ Unidad11/Dal/GestoraPersonas.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Entidades;
 
 namespace Dal
 {
     public class GestoraPersonas
     {
-        public static List<ClsPersona> obtenerPersonas() {
-            List<ClsPersona> listaPersonas = new List<ClsPersona>();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static ObservableCollection<ClsPersona> obtenerPersonas() {
+            ObservableCollection<ClsPersona> listaPersonas = new ObservableCollection<ClsPersona>();
 
             listaPersonas.Add(new ClsPersona("Persona1","Apellido1",20));
             listaPersonas.Add(new ClsPersona("Persona2", "Apellido2", 20));
