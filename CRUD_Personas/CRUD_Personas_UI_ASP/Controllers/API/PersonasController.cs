57227caed7d5a6051979f7a5e820e31427fb2a6c
Alejandro Ruiz Campos <alexruiz29del12@gmail.com>
2021-12-14 13:48:02 +0100

    CRUD Personas APS con ApiController

---
+++ CRUD_Personas/CRUD_Personas_UI_ASP/Controllers/API/PersonasController.cs
@@ -0,0 +1,50 @@
+﻿using CRUD_Personas_BL.Listados;
+using CRUD_Personas_Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CRUD_Personas_UI_ASP.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        // GET: api/<PersonasController>
+        [HttpGet]
+        public IEnumerable<ClsPersona> Get()
+        {
+            List<ClsPersona> listaPersonas = ListadosBL.obtenerPersonas();
+
+            return listaPersonas;
+        }
+        // GET api/<PersonasController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PersonasController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PersonasController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PersonasController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
